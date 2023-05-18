3633c69dad4f8552d0f30b44269d4ba12a185c0a
Edson Candido <Edson.Candido@howden.com>
2023-05-18 13:18:24 -0300

    added PaintProjectForm

---
+++ HSA_Tintas/Views/PaintProjectsForm.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace Controle_Tintas.Views
+{
+    partial class PaintProjectsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            labelUserName = new Label();
+            groupBox1 = new GroupBox();
+            flowLayoutPanel2 = new FlowLayoutPanel();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            flowLayoutPanel3 = new FlowLayoutPanel();
+            textBox2 = new TextBox();
+            button2 = new Button();
+            label3 = new Label();
+            flowLayoutPanel4 = new FlowLayoutPanel();
+            textBox3 = new TextBox();
+            button3 = new Button();
+            label4 = new Label();
+            flowLayoutPanel5 = new FlowLayoutPanel();
+            comboBox1 = new ComboBox();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            dataGridView1 = new DataGridView();
+            flowLayoutPanel1.SuspendLayout();
+            groupBox1.SuspendLayout();
+            flowLayoutPanel2.SuspendLayout();
+            flowLayoutPanel3.SuspendLayout();
+            flowLayoutPanel4.SuspendLayout();
+            flowLayoutPanel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(241, 37);
+            label1.TabIndex = 2;
+            label1.Text = "Tintas para projeto";
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Controls.Add(labelUserName);
+            flowLayoutPanel1.Controls.Add(flowLayoutPanel2);
+            flowLayoutPanel1.Controls.Add(label2);
+            flowLayoutPanel1.Controls.Add(flowLayoutPanel3);
+            flowLayoutPanel1.Controls.Add(label3);
+            flowLayoutPanel1.Controls.Add(flowLayoutPanel4);
+            flowLayoutPanel1.Controls.Add(label4);
+            flowLayoutPanel1.Controls.Add(flowLayoutPanel5);
+            flowLayoutPanel1.FlowDirection = FlowDirection.TopDown;
+            flowLayoutPanel1.Location = new Point(6, 22);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(252, 254);
+            flowLayoutPanel1.TabIndex = 3;
+            // 
+            // labelUserName
+            // 
+            labelUserName.AutoSize = true;
+            labelUserName.Location = new Point(3, 0);
+            labelUserName.Name = "labelUserName";
+            labelUserName.Size = new Size(104, 15);
+            labelUserName.TabIndex = 0;
+            labelUserName.Text = "Pesquisar por cód.";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button4);
+            groupBox1.Controls.Add(flowLayoutPanel1);
+            groupBox1.Location = new Point(12, 49);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(264, 322);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Filtros";
+            // 
+            // flowLayoutPanel2
+            // 
+            flowLayoutPanel2.Controls.Add(textBox1);
+            flowLayoutPanel2.Controls.Add(button1);
+            flowLayoutPanel2.Location = new Point(3, 18);
+            flowLayoutPanel2.Name = "flowLayoutPanel2";
+            flowLayoutPanel2.Size = new Size(249, 40);
+            flowLayoutPanel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(165, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 30);
+            button1.TabIndex = 0;
+            button1.Text = "Pesquisar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(3, 3);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(156, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(3, 61);
+            label2.Name = "label2";
+            label2.Size = new Size(119, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Pesquisar por projeto";
+            // 
+            // flowLayoutPanel3
+            // 
+            flowLayoutPanel3.Controls.Add(textBox2);
+            flowLayoutPanel3.Controls.Add(button2);
+            flowLayoutPanel3.Location = new Point(3, 79);
+            flowLayoutPanel3.Name = "flowLayoutPanel3";
+            flowLayoutPanel3.Size = new Size(249, 40);
+            flowLayoutPanel3.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(3, 3);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(156, 23);
+            textBox2.TabIndex = 1;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(165, 3);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 30);
+            button2.TabIndex = 0;
+            button2.Text = "Pesquisar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(3, 122);
+            label3.Name = "label3";
+            label3.Size = new Size(131, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Pesquisar por descrição";
+            // 
+            // flowLayoutPanel4
+            // 
+            flowLayoutPanel4.Controls.Add(textBox3);
+            flowLayoutPanel4.Controls.Add(button3);
+            flowLayoutPanel4.Location = new Point(3, 140);
+            flowLayoutPanel4.Name = "flowLayoutPanel4";
+            flowLayoutPanel4.Size = new Size(249, 40);
+            flowLayoutPanel4.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(3, 3);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(156, 23);
+            textBox3.TabIndex = 1;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(165, 3);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 30);
+            button3.TabIndex = 0;
+            button3.Text = "Pesquisar";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(3, 183);
+            label4.Name = "label4";
+            label4.Size = new Size(112, 15);
+            label4.TabIndex = 6;
+            label4.Text = "Pesquisar por status";
+            // 
+            // flowLayoutPanel5
+            // 
+            flowLayoutPanel5.Controls.Add(comboBox1);
+            flowLayoutPanel5.Location = new Point(3, 201);
+            flowLayoutPanel5.Name = "flowLayoutPanel5";
+            flowLayoutPanel5.Size = new Size(249, 40);
+            flowLayoutPanel5.TabIndex = 7;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "DISPONIVEL", "EM USO" });
+            comboBox1.Location = new Point(3, 3);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(156, 23);
+            comboBox1.TabIndex = 0;
+            comboBox1.Text = "DISPONIVEL";
+            // 
+            // button4
+            // 
+            button4.Location = new Point(75, 282);
+            button4.Name = "button4";
+            button4.Size = new Size(93, 30);
+            button4.TabIndex = 4;
+            button4.Text = "Limpar filtros";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(287, 56);
+            button5.Name = "button5";
+            button5.Size = new Size(174, 30);
+            button5.TabIndex = 5;
+            button5.Text = "Adiciona tinta para projeto";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(467, 56);
+            button6.Name = "button6";
+            button6.Size = new Size(174, 30);
+            button6.TabIndex = 6;
+            button6.Text = "Utilizar tinta";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(647, 56);
+            button7.Name = "button7";
+            button7.Size = new Size(174, 30);
+            button7.TabIndex = 7;
+            button7.Text = "Enviar tinta para sobra";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(287, 94);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(551, 532);
+            dataGridView1.TabIndex = 8;
+            // 
+            // PaintProjectsForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(850, 638);
+            Controls.Add(dataGridView1);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(groupBox1);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "PaintProjectsForm";
+            Text = "PaintProjectsForm";
+            flowLayoutPanel1.ResumeLayout(false);
+            flowLayoutPanel1.PerformLayout();
+            groupBox1.ResumeLayout(false);
+            flowLayoutPanel2.ResumeLayout(false);
+            flowLayoutPanel2.PerformLayout();
+            flowLayoutPanel3.ResumeLayout(false);
+            flowLayoutPanel3.PerformLayout();
+            flowLayoutPanel4.ResumeLayout(false);
+            flowLayoutPanel4.PerformLayout();
+            flowLayoutPanel5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Label labelUserName;
+        private FlowLayoutPanel flowLayoutPanel2;
+        private TextBox textBox1;
+        private Button button1;
+        private Label label2;
+        private FlowLayoutPanel flowLayoutPanel3;
+        private TextBox textBox2;
+        private Button button2;
+        private Label label3;
+        private FlowLayoutPanel flowLayoutPanel4;
+        private TextBox textBox3;
+        private Button button3;
+        private Label label4;
+        private FlowLayoutPanel flowLayoutPanel5;
+        private ComboBox comboBox1;
+        private GroupBox groupBox1;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private DataGridView dataGridView1;
+    }
+}
